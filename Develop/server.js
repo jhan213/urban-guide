4af7374ff9f851beedc29aeb2e43bc31d84772cc
jaehan213 <105683273+jaehan213@users.noreply.github.com>
2022-08-19 23:19:45 -0700

    fixed server.js to work with displaying notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,73 +1,82 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const uuid = require('./helpers/uuid');
 const notesData = require('./db/db.json');
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001;
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
-app.get('/notes', (req, res) => {
+  app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('/api/notes', (req, res) => res.status(200).json(notesData));
-
-app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a note`);
-    fs.readFile('./db/db.json', function (err, data) {
-        var json = JSON.parse(data)
-        json.push(req.body)
-        fs.writeFile("./db/db.json", JSON.stringify(json), err =>{
-            if(err) console.log(err)
-        })
-    })
-    const response = {
-        status: 'success',
-        body: req.body,
-      };
-  
-      res.status(201).json(response);
-    
-    // // Destructuring assignment for the items in req.body
-    // const { product, review, username } = req.body;
-  
-    // // If all the required properties are present
-    // if (product && review && username) {
-    //   // Variable for the object we will save
-    //   const newReview = {
-    //     product,
-    //     review,
-    //     username,
-    //     upvotes: Math.floor(Math.random() * 100),
-    //     review_id: uuid(),
-    //   };
-  
-    //   const response = {
-    //     status: 'success',
-    //     body: newReview,
-    //   };
-  
-    //   console.log(response);
-    //   res.status(201).json(response);
-    // } else {
-    //   res.status(500).json('Error in posting review');
-    // }
   });
+
+  const readFromFile = util.promisify(fs.readFile);
 
   app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to get notes`)
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+  app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    const { title, text } = req.body;
+    res.json(notesData);
+
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+          title,
+          text,
+          id: uuid(),
+        };
+    
+        // Obtain existing notes
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+            console.error(err);
+            } else {
+            // Convert string into JSON object
+            const parsedNotes = JSON.parse(data);
+
+            // Add a new note
+            parsedNotes.push(newNote);
+
+            // Write updated notes back to the file
+            fs.writeFile(
+                './db/db.json',
+                JSON.stringify(parsedNotes, null, 4),
+                (writeErr) =>
+                writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully updated notes!')
+            );
+            }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+  });
+
+app.delete(`/api/notes/:id`, (req, res) => {
+
+})
+
+  app.listen(PORT, () => {
+    console.log(`NoteTaker App listening at http://localhost:${PORT}`);
   });
